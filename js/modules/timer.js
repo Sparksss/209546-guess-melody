8f20f8a77b656b3876482e72bde997d645d9cb21
Ilya Rogatkin <toch.in.go@googlemail.com>
2018-04-09 10:50:11 +0300

    fix timer

--- js/modules/timer.js
+++ js/modules/timer.js
@@ -1,15 +1,19 @@
 const createTimeLimit = (time) => {
-  return {
-    timeLimit: time,
-    endOfTime: ``,
-    tick() {
-      if (this.timeLimit > 0) {
-        --this.timeLimit;
-      } else {
-        this.endOfTime = `time is over!`;
+  if (time) {
+    return {
+      timeLimit: time,
+      endOfTime: ``,
+      tick() {
+        if (this.timeLimit > 0) {
+          --this.timeLimit;
+        } else {
+          this.endOfTime = `time is over!`;
+        }
       }
-    }
-  };
+    };
+  } else {
+    return -1;
+  }
 };
 
 export default createTimeLimit;
