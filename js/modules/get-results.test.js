ca65b97398af8537d64644509ada7c00f3740e0e
Ilya Rogatkin <toch.in.go@googlemail.com>
2018-04-10 22:37:27 +0300

    add fill method for parameters

--- js/modules/get-results.test.js
+++ js/modules/get-results.test.js
@@ -2,8 +2,7 @@ import {assert} from "chai";
 import getResults from "./../modules/get-results";
 
 const answers = [];
-const answ = [];
-const notComplete = [true, true, true, true];
+const notComplete = [];
 const question = () => {
   return {
     answer: true,
@@ -13,19 +12,14 @@ const question = () => {
 for (let i = 10; i--;) {
   answers.push(question());
 }
-
-for (let i = 10; i--;) {
-  answ.push({answer: true});
-}
-
 describe(`Check user points`, () => {
   it(`should return -1 if answers less 10`, () => {
-    assert(getResults(notComplete) === -1, `number answers should be equal and greater than ten`);
-    assert.equal(-1, getResults(notComplete, 1), `quantity of answers less ten`);
+    assert(getResults([1, 2, 3].fill({answer: true})) === -1, `number answers should be equal and greater than ten`);
+    assert.equal(-1, getResults([1, 2, 3, 4, 5].fill({answer: true, timeLimit: 40}), 1), `quantity of answers less ten`);
     assert.notEqual(10, getResults(notComplete, 2), `optional testing :check number of quantity answers`);
   });
   it(`should return 10 if answers is true but time greater than 30 seconds for one question`, () => {
-    assert.equal(10, getResults(answ, 1));
+    assert.equal(10, getResults([1, 2, 3, 4, 5, 6, 7, 8, 9, 10].fill({answer: true, timeLimit: 40}), 1));
   });
   it(`should return if sum points greater than ten`, () => {
     assert(getResults(answers, 1) >= 10, `error`);
