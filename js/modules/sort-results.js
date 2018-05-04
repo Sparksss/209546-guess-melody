c8bca43783e872947abe3b485e2326c8a3a05a0c
Ilya Rogatkin <toch.in.go@googlemail.com>
2018-05-04 10:30:58 +0300

    changed sort-results

--- js/modules/sort-results.js
+++ js/modules/sort-results.js
@@ -21,8 +21,6 @@ const sortGameResults = (statistics, {points, notes, time} = {}) => {
   const stat = {
     playerPosition,
     quantityPoints,
-    time,
-    notes
   };
 
   return stat;
@@ -32,13 +30,8 @@ const renderStringResult = (results) => {
   if (Object.keys(results).length === 0) {
     return -1;
   }
-  const {playerPosition, quantityPoints, time, notes} = results;
+  const {playerPosition, quantityPoints} = results;
   let message = `Вы заняли ${playerPosition + CORRECT_POSITION_IN_ARRAY} место из ${quantityPoints} игроков. Это лучше, чем у ${(quantityPoints - playerPosition) * 10}% игроков`;
-  if (!time === TIME_OUT) {
-    message = `Время вышло! Вы не успели отгадать все мелодии!`;
-  } else if (!notes) {
-    message = `У вас закончились все попытки. Ничего, повезёт в следующий раз!`;
-  }
   return message;
 };
 
